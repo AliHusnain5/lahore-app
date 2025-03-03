73d3e862554461b584202c52382a6ba72114d28e
AliHusnain5 <ah4008189@gmail.com>
2025-03-03 10:53:56 +0500

    this

--- src/components/FristPage.jsx
+++ src/components/FristPage.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const FristPage = () => {
   return (
     <div>
-<h1>degree college</h1>      
+<h1>degree college</h1> 
+<button>junaid ahmed</button> 
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, maxime.</p>    
     </div>
   )
 }
