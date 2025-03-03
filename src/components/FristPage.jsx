85ae83095695dc56899d7b4c10abc79c2d567412
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-03-03 10:50:38 +0500

    input create in firstpage

--- src/components/FristPage.jsx
+++ src/components/FristPage.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const FristPage = () => {
   return (
     <div>
-<h1>degree college</h1>      
+<h1>degree college</h1>   
+Search:<input type='search'></input>
+<h2 style={{backgroundColor:"green", fontSize:"56px"}}>Muhammad Zain Naseer</h2>   
     </div>
   )
 }
