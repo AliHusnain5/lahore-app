dd74922f81ea5c5bafe28a5fc46be4f1f9f90e94
AliHusnain5 <ah4008189@gmail.com>
2025-03-03 11:23:24 +0500

    new files

---
+++ src/components/Confilect.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Confilect = () => {
+  return (
+    <div>
+      <input type='text ' placeholder='search'></input>
+      <button>faisalabad</button>
+    </div>
+  )
+}
+
+export default Confilect
